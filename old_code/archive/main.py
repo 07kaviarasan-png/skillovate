c70a98d0377755626703cf9958e594871c02f964
Billa <adithyen1@gmail.com>
2026-06-21 20:39:05 +0530

    refactor: remove legacy code and outdated graphify cache files

--- old_code/archive/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from skillovate!")
-
-
-if __name__ == "__main__":
-    main()
--- old_code/interview.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { getInterviewHistory, submitInterview } = require('./interview.controller');
-const auth = require('./auth');
-const { collegeScope } = require('./rbac');
-
-router.get('/:id/interviews', auth, collegeScope, getInterviewHistory);
-router.post('/:id/interviews', auth, submitInterview);
-
-module.exports = router;
